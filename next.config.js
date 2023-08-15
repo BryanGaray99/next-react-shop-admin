e7ce699fa7eef0de5e9e9b3130c8b4e1dc7a0932
BryanGaray <bryangarayacademico@gmail.com>
2023-08-14 20:27:55 -0500

    [UPDATE] next.config.js ignoring esLint during prod build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   images: {
     domains: ['upload.wikimedia.org', 'picsum.photos', '**', 'ui-avatars.com' ]
   }
